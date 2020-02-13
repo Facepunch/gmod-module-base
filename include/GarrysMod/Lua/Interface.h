be385acdd42c3ca2bab0b862c5d860fee548dec9
William Wallace <me@wiox.me>
2020-02-13 00:06:32 +0000

    update from gmod

--- include/GarrysMod/Lua/Interface.h
+++ include/GarrysMod/Lua/Interface.h
@@ -37,40 +37,39 @@ struct lua_State
         #define DLL_EXPORT extern "C" __attribute__((visibility("default")))
     #endif
 
-#ifdef GMOD_ALLOW_DEPRECATED
-    // Stop using this and use LUA_FUNCTION!
-    #define LUA ( state->luabase )
+    #ifdef GMOD_ALLOW_DEPRECATED
+        // Stop using this and use LUA_FUNCTION!
+        #define LUA ( state->luabase )
 
-    #define GMOD_MODULE_OPEN()  DLL_EXPORT int gmod13_open( lua_State* state )
-    #define GMOD_MODULE_CLOSE() DLL_EXPORT int gmod13_close( lua_State* state )
-#else
-    #define GMOD_MODULE_OPEN()                     \
-        int gmod13_open__Imp( GarrysMod::Lua::ILuaBase* LUA );     \
-        DLL_EXPORT int gmod13_open( lua_State* L ) \
-        {                                          \
-            return gmod13_open__Imp( L->luabase ); \
-        }                                          \
-        int gmod13_open__Imp( GarrysMod::Lua::ILuaBase* LUA )
+        #define GMOD_MODULE_OPEN()  DLL_EXPORT int gmod13_open( lua_State* state )
+        #define GMOD_MODULE_CLOSE() DLL_EXPORT int gmod13_close( lua_State* state )
+    #else
+        #define GMOD_MODULE_OPEN()                                 \
+            int gmod13_open__Imp( GarrysMod::Lua::ILuaBase* LUA ); \
+            DLL_EXPORT int gmod13_open( lua_State* L )             \
+            {                                                      \
+                return gmod13_open__Imp( L->luabase );             \
+            }                                                      \
+            int gmod13_open__Imp( GarrysMod::Lua::ILuaBase* LUA )
 
-    #define GMOD_MODULE_CLOSE()                     \
-        int gmod13_close__Imp( GarrysMod::Lua::ILuaBase* LUA );     \
-        DLL_EXPORT int gmod13_close( lua_State* L ) \
-        {                                           \
-            return gmod13_close__Imp( L->luabase ); \
-        }                                           \
-        int gmod13_close__Imp( GarrysMod::Lua::ILuaBase* LUA )
+        #define GMOD_MODULE_CLOSE()                                 \
+            int gmod13_close__Imp( GarrysMod::Lua::ILuaBase* LUA ); \
+            DLL_EXPORT int gmod13_close( lua_State* L )             \
+            {                                                       \
+                return gmod13_close__Imp( L->luabase );             \
+            }                                                       \
+            int gmod13_close__Imp( GarrysMod::Lua::ILuaBase* LUA )
 
-    #define LUA_FUNCTION( FUNC )          \
-        int FUNC##__Imp( GarrysMod::Lua::ILuaBase* LUA ); \
-        int FUNC( lua_State* L )          \
-        {                                 \
-            GarrysMod::Lua::ILuaBase* LUA = L->luabase;   \
-            LUA->SetState(L);             \
-            return FUNC##__Imp( LUA );    \
-        }                                 \
-        int FUNC##__Imp( GarrysMod::Lua::ILuaBase* LUA )
-#endif
-
+        #define LUA_FUNCTION( FUNC )                          \
+            int FUNC##__Imp( GarrysMod::Lua::ILuaBase* LUA ); \
+            int FUNC( lua_State* L )                          \
+            {                                                 \
+                GarrysMod::Lua::ILuaBase* LUA = L->luabase;   \
+                LUA->SetState(L);                             \
+                return FUNC##__Imp( LUA );                    \
+            }                                                 \
+            int FUNC##__Imp( GarrysMod::Lua::ILuaBase* LUA )
+    #endif
 #endif
 
 #endif
